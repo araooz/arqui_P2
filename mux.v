8c4e8939f06f20597bf9e4fc2e216df974534fc4
araooz <araozfali@gmail.com>
2026-06-23 12:25:04 -0500

    refactor: unificados los muxes en un solo archivo

---
+++ mux.v
@@ -0,0 +1,29 @@
+module mux2 (input  [WIDTH-1:0] d0, d1, 
+              input  s, 
+              output [WIDTH-1:0] y);
+
+  parameter WIDTH = 8;
+
+  assign y = s ? d1 : d0; 
+endmodule
+
+module mux3 (input  [WIDTH-1:0] d0, d1, d2,
+              input  [1:0]       s, 
+              output [WIDTH-1:0] y);
+
+  parameter WIDTH = 8;
+
+  assign y = s[1] ? d2 : (s[0] ? d1 : d0); 
+endmodule
+
+module mux4 (input  [WIDTH-1:0] d0, d1, d2, d3,
+              input  [1:0]       s,
+              output [WIDTH-1:0] y);
+
+  parameter WIDTH = 8;
+
+  assign y = s[1] ? 
+               (s[0] ? d3 : d2) :
+               (s[0] ? d1 : d0);
+
+endmodule
